1dc7e07dd502a7fe5f42365ae2d3629314152087
BasiaDziwoki <bd305970@student.polsl.pl>
2025-06-25 21:18:29 +0200

    added sucking portals

--- Source/PortalGame/Portal.cpp
+++ Source/PortalGame/Portal.cpp
@@ -15,62 +15,6 @@ APortal::APortal()
 
 }
 
-//void APortal::BeginOverlap1(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-//{
-//	FVector TargetPoint = GetTargetPoint(SweepResult, TP2, TP1);
-//	//JumpPortalM(OtherActor, Col1, P1Capture, OverlappedComponent, TargetPoint);
-//}
-//
-//void APortal::BeginOverlap2(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-//{
-//	FVector TargetPoint = GetTargetPoint(SweepResult, TP1, TP2);
-//	//JumpPortalM(OtherActor, Col2, P2Capture, OverlappedComponent, TargetPoint);
-//}
-//
-//void APortal::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
-//{
-//	if (OverlappedComponent == ExitCollider)
-//	{
-//		ExitCollider = nullptr;
-//	}
-//}
-
-// Called when the game starts or when spawned
-void APortal::BeginPlay()
-{
-	Super::BeginPlay();
-	P1Capture = GetP1CaptureComp();
-	P2Capture = GetP2CaptureComp();
-	PSim_D1 = GetPSim_D1Comp();
-	PSim_D2 = GetPSim_D2Comp();
-	Col1 = GetCol1Comp();
-	Col2 = GetCol2Comp();
-	DummyPortal = GetDummyPortalComp();
-	U = GetUComp();
-	D = GetDComp();
-	L = GetLComp();
-	R = GetRComp();
-	TP1 = GetTP1Comp();
-	TP2 = GetTP2Comp();
-	ExitCollider = GetExitCollider();
-	Col1->SetGenerateOverlapEvents(true);
-	Col2->SetGenerateOverlapEvents(true);
-	FScriptDelegate ScriptDelegate1;
-	ScriptDelegate1.BindUFunction(this, "EndOverlap");
-	Col1->OnComponentEndOverlap.Add(ScriptDelegate1);
-	FScriptDelegate ScriptDelegate2;
-	ScriptDelegate2.BindUFunction(this, "EndOverlap");
-	Col2->OnComponentEndOverlap.Add(ScriptDelegate2);
-	FScriptDelegate ScriptDelegate3;
-	ScriptDelegate3.BindUFunction(this, "BeginOverlap2");
-	Col1->OnComponentBeginOverlap.Add(ScriptDelegate3);
-	FScriptDelegate ScriptDelegate4;
-	ScriptDelegate4.BindUFunction(this, "BeginOverlap1");
-	Col2->OnComponentBeginOverlap.Add(ScriptDelegate4);
-
-
-
-}
 
 // Called every frame
 void APortal::Tick(float DeltaTime)
@@ -137,5 +81,18 @@ void APortal::Tick(float DeltaTime)
 //	double FOV = UKismetMathLibrary::Clamp(UKismetMathLibrary::Atan(UKismetMathLibrary::Divide_DoubleDouble(200, UKismetMathLibrary::Max(VectorLength, 1))), 5, 100);
 //	Capture->FOVAngle = FOV;
 //}
+
+void APortal::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Jeœli masz komponent, np. CollisionComponent:
+	// if (CollisionComponent)
+	// {
+	//     CollisionComponent->SetGenerateOverlapEvents(true);
+	// }
+
+	// Na razie pusty — byle istnia³!
+}
 
 
